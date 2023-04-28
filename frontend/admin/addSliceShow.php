278a1bb0b7d7d93d65d9f12fce12a227b3877668
KahtesSSELA <124881203+KahtesSSELA@users.noreply.github.com>
2023-04-28 18:48:29 +0700

    +Change -Admin :add Product Form

---
+++ frontend/admin/addSliceShow.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<?php include "components/head.php" ?>
+
+<body>
+<div class="wrapper">
+	<?php include "components/header.php" ?>
+  	<?php include "components/sidebar.php" ?>
+
+  	<?php include "components/SliceShowContent.php" ?>
+
+	<?php include "components/footer.php" ?>
+</div>
+<!-- ./wrapper -->
+
+</body>
+</html>
