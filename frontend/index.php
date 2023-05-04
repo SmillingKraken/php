268e2a55cf74bc51756ce41f78fa4931c983a48f
Tosa <cornortosa007@gmail.com>
2023-05-04 16:27:05 +0700

    Login Register complete

--- frontend/index.php
+++ frontend/index.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
 
-<?php 
+<?php
 
-	// php goes here
+// php goes here
 
-?> 
+?>
 
 
 <html>
 
 <?php include "components/head.php" ?>
 
+<?php
+require 'config.php';
+if(!empty($_SESSION["id"])){
+  $id = $_SESSION["id"];
+  $result = mysqli_query($conn, "SELECT * FROM tb_user WHERE id = $id");
+  $row = mysqli_fetch_assoc($result);
+}
+else{
+}
+?>
+
 <body>
 
-	<?php include "components/header.php" ?>
+	<?php include "./components/header.php" ?>
 	<?php include "components/product.php" ?>
 	<?php include "components/footer.php" ?>
 
