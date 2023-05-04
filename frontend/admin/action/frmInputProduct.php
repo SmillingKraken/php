8f7169ace418653597421e2e111ea8b3003cd8d8
KahtesSSELA <124881203+KahtesSSELA@users.noreply.github.com>
2023-05-04 16:52:31 +0700

    Update frmInputProduct.php

--- frontend/admin/action/frmInputProduct.php
+++ frontend/admin/action/frmInputProduct.php
@@ -3,22 +3,7 @@ require("./database.php");
 
 
 if ($_POST) {
-  //
-  $target_dir = "uploads/";
-  $target_file = $target_dir . basename($_FILES["img"]["name"]);
-  $uploadOk = 1;
-  $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-  if (isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["img"]["tmp_name"]);
-    if ($check !== false) {
-      echo "File is an image - " . $check["mime"] . ".";
-      $uploadOk = 1;
-    } else {
-      echo "File is not an image.";
-      $uploadOk = 0;
-    }
-  }
-  echo 
+ 
   //
 
   $pname = trim($_POST['pName']);
